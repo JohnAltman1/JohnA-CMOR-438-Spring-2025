443603c1463b601467cb8bf97859c98d0af5fae5
John Altman <jca4@rice.edu>
2025-05-06 05:15:04 -0500

    ensemble

---
+++ Supervised Learning/Ensemble/README.md
@@ -0,0 +1,12 @@
+# Ensemble Models Implementation
+
+This folder contains implementations of various ensemble learning algorithms.
+
+## Dataset
+
+The ensemble models are applied to the **Banknote Authentication** dataset, sourced from the [UC Irvine Machine Learning Repository](https://archive.ics.uci.edu/dataset/267/banknote+authentication).
+
+## Notebook Contents
+
+The **Ensemble Models Notebook** in this folder contains explanations of ensemble learning techniques, including random forests, and demonstrations of their use on the **Banknote Authentication** dataset.
+
--- mltoolbox/knn.py
+++ mltoolbox/knn.py
@@ -2,10 +2,11 @@ import numpy as np
 
 
 class KNN(object):
-    def __init__(self, X_train, y_train, distance):
+    def __init__(self, X_train, y_train, distance, k=5):
         self.x = X_train
         self.y = y_train
         self.distance = distance
+        self.k = k
 
     def k_nearest_neighbors(self, point, k):
         # Create an empty list to store neighbors and distances
@@ -30,6 +31,7 @@ class KNN(object):
         
         else:
             return sum(x[1] for x in neighbors)/k
+
         
     def classification_error(self, test_features, test_labels, k):
         error = 0
